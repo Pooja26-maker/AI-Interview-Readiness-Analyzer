750da3fc9107e6e9471ff0d0bec57562a63ddb18
Pooja26-maker <vainavi.s.2016@gmail.com>
2026-06-14 16:19:09 +0530

    Add a list of skills to skills.py

---
+++ skills.py
@@ -0,0 +1,19 @@
+skills_list = [
+    "python","java","c","c++","javascript","typescript",
+    "html","css","react","angular","vue",
+    "nodejs","express","mongodb","mysql","postgresql",
+    "sql","firebase","redis",
+    "git","github","docker","kubernetes",
+    "aws","azure","gcp",
+    "streamlit","flask","django","fastapi",
+    "machine learning","deep learning","nlp",
+    "artificial intelligence","llm",
+    "data structures","algorithms",
+    "oops","problem solving",
+    "rest api","graphql",
+    "linux","networking",
+    "communication","leadership",
+    "teamwork","critical thinking",
+    "testing","debugging",
+    "agile","scrum"
+]
